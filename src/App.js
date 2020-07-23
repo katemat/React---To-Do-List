6d34ce7ef324ed5a04a6ad79371b8a51f49762b5
Katerina <matveevakaterinka@gmail.com>
2020-07-23 12:25:12 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ToDoItem from './components/ToDoItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-list">
+      <h1>My To Do List</h1>
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
     </div>
   );
 }
---
+++ src/components/ToDoItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ToDoItem.css';
+
+export default function ToDoItem() {
+  return (
+    <div className="todo-item">
+      <input type="checkbox" />
+      <p>Placeholder</p>
+    </div>
+  );
+}
